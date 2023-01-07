77fee0acc3afaf3743adbd3b82dda70120dc137f
kapi117 <69985093+kapi117@users.noreply.github.com>
2023-01-07 22:09:15 +0100

    Implementation of .hpp files

--- include/storage_types.hpp
+++ include/storage_types.hpp
@@ -6,4 +6,70 @@
  * i PackageQueue oraz typu wyliczeniowego PackageQueueType
 */
 
+#include <list>
+#include "package.hpp"
+#include "types.hpp"
+
+enum class PackageQueueType {
+    FIFO,
+    LIFO
+};
+
+/*!
+ * IPackageStockpile
+ */
+class IPackageStockpile {
+public:
+    using const_iterator = std::list<Package>::const_iterator;
+
+    virtual void push(Package &&) = 0;
+
+    virtual bool empty() const = 0;
+
+    virtual size_t size() const = 0;
+
+    virtual const_iterator cbegin() const = 0;
+
+    virtual const_iterator cend() const = 0;
+
+    virtual const_iterator begin() const = 0;
+
+    virtual const_iterator end() const = 0;
+
+    virtual ~IPackageStockpile() = 0;
+};
+
+class IPackageQueue : public IPackageStockpile {
+public:
+    virtual Package pop() = 0;
+
+    virtual PackageQueueType get_queue_type() const = 0;
+};
+
+class PackageQueue : public IPackageQueue {
+public:
+    PackageQueue(PackageQueueType type) : type_(type) {};
+
+    void push(Package &&) override;
+
+    bool empty() const override;
+
+    size_t size() const override;
+
+    const_iterator cbegin() const override;
+
+    const_iterator cend() const override;
+
+    const_iterator begin() const override;
+
+    const_iterator end() const override;
+
+    Package pop() override;
+
+    PackageQueueType get_queue_type() const override;
+
+private:
+    PackageQueueType type_;
+};
+
 #endif //NETSIM_STORAGE_TYPES_HPP
