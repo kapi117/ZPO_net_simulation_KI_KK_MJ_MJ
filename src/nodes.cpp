370454122be50bb662bf4c8b817ada2fb1d30ef8
Kacper Kaczmarski <kkaczmarski.npg@gmail.com>
2023-01-16 17:38:56 +0100

    Done do_work()

--- src/nodes.cpp
+++ src/nodes.cpp
@@ -5,12 +5,17 @@
 #include "nodes.hpp"
 
 void Worker::do_work(Time t) {
-    if (package_processing_start_time_ == 0){
-        package_processing_start_time_ = t;
-    }
-    if (package_processing_start_time_ + pd_ == t) {
-        send_package();
-        push_package(package_queue_->pop());
-        package_processing_start_time_ = 0;
+    if(!sending_buffer_) {
+        if (package_processing_start_time_ + pd_ == t) {
+            send_package();
+            package_processing_start_time_ = 0;
+        }
+    } else {
+        if (not package_queue_->empty()) {
+            push_package(package_queue_->pop());
+            if (package_processing_start_time_ == 0) {
+                package_processing_start_time_ = t;
+            }
+        }
     }
 }
